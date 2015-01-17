a02c7c83f510d4b44a80b72ba25655788c58840d
Jacob Walker <jacob@tortugas-llc.com>
2015-01-17 14:29:35 -0600

    Add about option

--- cmd/post-hydra.go
+++ cmd/post-hydra.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/jcbwlkr/terminus"
 	"github.com/nsf/termbox-go"
+	"time"
 )
 
 func main() {
@@ -11,14 +12,26 @@ func main() {
 	}
 	defer termbox.Close()
 
-	menu := new(terminus.Menu)
+	app := terminus.NewApp(termbox.ColorWhite, termbox.ColorBlack)
+
+	menu := terminus.NewMenu(app)
 
 	menu.Title = `
 POST HYDRA
 `
-	menu.Options = make([]terminus.MenuOption, 1)
-	menu.Options[0] = terminus.NewExitOption("Quit")
+	menu.AddOption(&terminus.MenuOption{"About", about})
+	menu.AddOption(terminus.NewExitOption("Quit"))
 
 	menu.Run()
+}
 
+func about(app *terminus.App) int {
+	app.Clear()
+	app.DrawLine("Post Hydra!", 1, 1)
+	app.DrawLine("This is an app!", 1, 3)
+	termbox.Flush()
+
+	time.Sleep(1 * time.Second)
+
+	return terminus.Continue
 }
